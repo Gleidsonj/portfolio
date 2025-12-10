0c5b40edd84ce5bf7a8fd1dcccc6cff778977337
gleidson <gleidsonlimacrf@gmail.com>
2025-12-10 19:30:57 -0300

    melhorando secao sobre mim

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -8,7 +8,7 @@ function Sobre() {
     return (
         <section id='sobre' className='secao-sobre'>
             <section className='sobre-mim'>
-                <h2>Sobre Mim</h2>
+                <h2>Sobre mim</h2>
                 <div className='container-1'>
                     <div className='secao-1'>
                         <p>Sou Gleidson Lima, tenho 23 anos e estudo Análise e Desenvolvimento de Sistemas. Estou construindo minha carreira como desenvolvedor full stack, criando projetos práticos e bem estruturados. Faço front com HTML, CSS, JavaScript, TypeScript e React; no back uso Node.js, Express e MySQL para APIs e bancos de dados. Trabalho com Git e GitHub e estou sempre aprendendo novas ferramentas para entregar soluções mais completas.</p>
