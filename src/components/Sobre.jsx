9614e2df1abd9388426ab8ef49d58d4018df2fcf
gleidson <gleidsonlimacrf@gmail.com>
2025-12-10 19:51:40 -0300

    corrigindo erros

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -12,17 +12,17 @@ function Sobre() {
                 <div className='container-1'>
                     <div className='secao-1'>
                         <p>Sou Gleidson Lima, tenho 23 anos e estudo Análise e Desenvolvimento de Sistemas. Estou construindo minha carreira como desenvolvedor full stack, criando projetos práticos e bem estruturados. Faço front com HTML, CSS, JavaScript, TypeScript e React; no back uso Node.js, Express e MySQL para APIs e bancos de dados. Trabalho com Git e GitHub e estou sempre aprendendo novas ferramentas para entregar soluções mais completas.</p>
-                        <div className='secao-2'>
-                            <div>
-                                <FontAwesomeIcon icon={faBriefcase} id='iconColor' size='2x' />
-                                <h3>1+</h3>
-                                <p>Anos de experiência</p>
-                            </div>
-                            <div>
-                                <FontAwesomeIcon icon={faBriefcase} id='iconColor' size='2x' />
-                                <h3>3+</h3>
-                                <p>Projetos pessoais</p>
-                            </div>
+                    </div>
+                    <div className='secao-2'>
+                        <div>
+                            <FontAwesomeIcon icon={faBriefcase} id='iconColor' size='2x' />
+                            <h3>1+</h3>
+                            <p>Anos de experiência</p>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faBriefcase} id='iconColor' size='2x' />
+                            <h3>3+</h3>
+                            <p>Projetos pessoais</p>
                         </div>
                     </div>
                 </div>
